fd46e98ad59c5bc99fa155bcae0158a98eadd475
beafig <beafigueroa89@gmail.com>
2023-03-31 11:31:15 +0200

    Add conecction to database

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
@@ -11,3 +12,34 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+// conectar a la base datos 
+mysql
+  .createConnection({
+    host: 'localhost',
+    database: 'netflix',
+    user: 'root',
+    password: '4467',
+  })
+  .then(connection => {
+    connection
+      .connect()
+      .then(() => {
+        return connection.query('SELECT * FROM movies');
+      })
+      .catch((err) => {
+        console.error('Error de conexion: ' + err.stack);
+      });
+  })
+  .catch((err) => {
+    console.error('Error de configuración: ' + err.stack);
+  });
+
+//petición al servidor
+server.get('/movies', (req, res) => {
+  res.send({
+    success: true,
+    movies: mysql
+  })
+  console.log(mysql);
+})
